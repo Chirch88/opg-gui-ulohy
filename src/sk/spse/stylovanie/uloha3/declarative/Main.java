f7736143ab57705771753f83e8149d7e46ad65d1
Chirch88 <73ondreurda@gmail.com>
2026-04-10 09:53:02 +0200

    posielam dnesnu pracu na hodine

---
+++ src/sk/spse/stylovanie/uloha3/declarative/Main.java
@@ -0,0 +1,7 @@
+package sk.spse.stylovanie.uloha3.declarative;
+
+public class Main {
+    public static void main(String[] args) {
+        sk.spse.stylovanie.uloha3.declarative.Application.launch(Application.class, args);
+    }
+}
